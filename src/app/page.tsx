b730d3460ebc7a14235cc7ef4389a8af6e0090b7
bimoware <ptdrrrrr.t.ki@gmail.com>
2025-04-06 10:16:11 +0100

    added languages and shi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Redacted from "@/cpnts/Redacted";
 import ScrollBottomArrow from "@/cpnts/ScrollBottomArrow";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
+
+	const t = useTranslations('HomePage');
+
 	return <>
 		<div className="w-screen h-screen flex items-center justify-around gap-10 p-10">
 			<div className="relative min-w-60 w-1/3 aspect-square group">
@@ -13,24 +17,23 @@ export default function Home() {
 							width="1000"
 							height="1000"
 							src={bool ? "/pfp.png" : "/pfp2.png"}
-							alt="Profile"
+							alt={t("pfp_alt")}
 							className={`absolute inset-0 w-full h-full object-cover transition-opacity
 								rounded-4xl select-none
 								${!bool && "opacity-0 group-hover:opacity-100 group-hover:duration-300"}`}
-						/>
-						)
+						/>)
 				}
 			</div>
 			<div>
 				<h1 className="text-4xl font-bold mb-5">
-					If you don't like basic websites, me neither.
+					{t("title")}
 				</h1>
 				<h3>
-					Hi! My name is <Redacted>Bimo</Redacted>, I'm 18 and a passionate web dev freelancer from Morocco. I enjoy making websites using Next.js
+					{t("subtitle")}
 				</h3>
 			</div>
 		</div>
-		<div className="absolute bottom-2 flex justify-center items-end w-screen h-screen">
+		<div className="absolute bottom-2 flex justify-center items-end w-screen">
 			<ScrollBottomArrow />
 		</div>
 	</>
